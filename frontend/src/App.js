3d7410d3fcf94f19f65b79543edef45f051b3701
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2020-12-16 12:04:09 +0530

    Home-page product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
-    <>
+    <div className="main-container">
       <Header />
       <main>
-        <Container>
+        <Container style={{marginTop: "5rem"}}>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../../src/products";
+import Product from '../components/Product'
 
 const HomeScreen = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  console.log(products, "PRoducts");
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
