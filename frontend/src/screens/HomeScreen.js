b0212addc2f061d8fae611b0cf6d0dcec30677b5
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2020-12-18 17:19:14 +0530

    serving and fetching data from express

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,28 @@
-import React, { Suspense, lazy } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../../src/products";
-import Product from '../components/Product'
+import axios from "axios";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h2 className="Blue-underline">Latest Products</h2>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
+            <Product product={product} />
           </Col>
         ))}
       </Row>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
+import { Row, Col, ListGroup, Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "../components/Rating";
-import products from "../products";
-import LazyImage from '../common/LazyImage'
+import LazyImage from "../common/LazyImage";
 
 const ProductScreen = ({ match, color }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match]);
 
   return (
     <div className="product-details-wrapper">
