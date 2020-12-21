c18c282bbb435950e4ae519584f488fef6db0922
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2020-12-21 16:32:21 +0530

    custom authentication middleware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import generateToken from '../utils/generateToken.js';
+import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -12,12 +12,32 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
   } else {
-     res.status(401)
-     throw new Error('Invalid email or password') 
+    res.status(401);
+    throw new Error("Invalid email or password");
   }
 });
 
-export { authUser };
+// @desc get user profile
+// @route GET /api/users/profile
+// @access PRIVATE
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+      res.status(404)
+      throw new Error('User not found')
+  }
+});
+
+export { authUser, getUserProfile };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //this user object will be able to all protected routes after this middleware gets executed
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not Authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorised, no token");
+  }
+});
+
+export { protect };
