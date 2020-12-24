ed690b1895682873cc3e788a29886fc5645fc700
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2020-12-24 17:26:24 +0530

    order controller added to create order

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import connectDB from './config/db.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js' 
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes )
+app.use('/api/orders', orderRoutes)
 
 //Below two functions are error-hanlding middlewares in JSON format
 
