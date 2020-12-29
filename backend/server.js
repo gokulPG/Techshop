b7d51c78b5a41709f90f6be7a6f0015ca4829503
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2020-12-29 11:51:56 +0530

    Image upload config and endpoint setup done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
@@ -5,6 +6,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -20,6 +22,11 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 //when user is ready to make payment, He will hit this url from frontend and fetch the client id when needed
 app.use("/api/config/paypal", (req, res) =>
